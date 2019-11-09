911177713badb4d1df80c6a817bd295094709cdc
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-09 21:50:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
-number = 10
 def countdown
+  number = 10
   while number > 1 
   puts "#{number} SECOND(S)!"
   number -= 1
-else puts "HAPPY NEW YEAR!"
-end
+puts "HAPPY NEW YEAR!"
 end
 end
